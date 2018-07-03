b022c4c3c097de3c8f6b0f49e55e2c70076ab412
Brent Christensen <brent@eventespresso.com>
2018-07-03 11:59:43 -0700

    fix formatting

--- eea-recurring-events.php
+++ eea-recurring-events.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Event Espresso - Recurring Events Manager (EE 4.9+)
   Plugin URI: http://www.eventespresso.com
-  Description: The Event Espresso Recurring Events addon...
+  Description: The Event Espresso Recurring Events Manager addon...
   Version: 1.0.0.dev.000
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
@@ -52,7 +52,7 @@ function espresso_recurring_events_plugin_activation_errors()
     // \EEH_Debug_Tools::printr(__FUNCTION__, __CLASS__, __FILE__, __LINE__, 2);
     if (WP_DEBUG) {
         $activation_errors = ob_get_contents();
-        if (! empty($activation_errors)) {
+        if (!empty($activation_errors)) {
             file_put_contents(
                 EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . 'espresso_recurring_events_plugin_activation_errors.html',
                 $activation_errors
@@ -60,8 +60,8 @@ function espresso_recurring_events_plugin_activation_errors()
         }
     }
 }
-add_action('activate_eea-recurring-events-manager/eea-recurring-events-manager.php', 'espresso_recurring_events_plugin_activation_errors');
 
+add_action('activate_eea-recurring-events-manager/eea-recurring-events-manager.php', 'espresso_recurring_events_plugin_activation_errors');
 
 
 /**
@@ -78,16 +78,16 @@ function load_espresso_recurring_events()
         try {
             EE_Psr4AutoloaderInit::psr4_loader()->addNamespace('EventEspresso\RecurringEvents', __DIR__);
             EE_Dependency_Map::register_dependencies(
-                'EventEspresso\RecurringEvents\domain\RecurringEventsManager',
+                'EventEspresso\RecurringEvents\src\domain\RecurringEventsManager',
                 array(
-                    'EE_Dependency_Map'                           => EE_Dependency_Map::load_from_cache,
-                    'EventEspresso\RecurringEvents\domain\Domain' => EE_Dependency_Map::load_from_cache,
+                    'EE_Dependency_Map' => EE_Dependency_Map::load_from_cache,
+                    'EventEspresso\RecurringEvents\src\domain\Domain' => EE_Dependency_Map::load_from_cache,
                 )
             );
-            EventEspresso\RecurringEvents\domain\RecurringEventsManager::registerAddon(
+            EventEspresso\RecurringEvents\src\domain\RecurringEventsManager::registerAddon(
                 EventEspresso\core\domain\DomainFactory::getShared(
                     new EventEspresso\core\domain\values\FullyQualifiedName(
-                        'EventEspresso\RecurringEvents\domain\Domain'
+                        'EventEspresso\RecurringEvents\src\domain\Domain'
                     ),
                     array(
                         new EventEspresso\core\domain\values\FilePath(EE_REM_PLUGIN_FILE),
@@ -97,16 +97,17 @@ function load_espresso_recurring_events()
             );
         } catch (Exception $e) {
             add_action(
-                    'admin_notices',
-                    function() use ($e) {
-                        echo '<div class="error"><p>' . $e->getMessage() . '</p></div>';
-                    }
+                'admin_notices',
+                function () use ($e) {
+                    echo '<div class="error"><p>' . $e->getMessage() . '</p></div>';
+                }
             );
         }
     } else {
         add_action('admin_notices', 'espresso_recurring_events_activation_error');
     }
 }
+
 add_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_recurring_events');
 
 
@@ -115,10 +116,11 @@ add_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_recurring_eve
  */
 function espresso_recurring_events_activation_check()
 {
-    if (! did_action('AHEE__EE_System__load_espresso_addons')) {
+    if (!did_action('AHEE__EE_System__load_espresso_addons')) {
         add_action('admin_notices', 'espresso_recurring_events_activation_error');
     }
 }
+
 add_action('init', 'espresso_recurring_events_activation_check', 1);
 
 
@@ -128,18 +130,18 @@ add_action('init', 'espresso_recurring_events_activation_check', 1);
 function espresso_recurring_events_activation_error()
 {
     unset($_GET['activate'], $_REQUEST['activate']);
-    if (! function_exists('deactivate_plugins')) {
+    if (!function_exists('deactivate_plugins')) {
         require_once ABSPATH . 'wp-admin/includes/plugin.php';
     }
     deactivate_plugins(plugin_basename(__FILE__));
     $error_message = sprintf(
-            esc_html__(
-                'Event Espresso Recurring Events addon could not be activated. Please ensure that Event Espresso version %1$s or higher is activated and your server is running PHP version %2$s or greater.',
-                'event_espresso'
-            ),
-            EE_REM_CORE_VERSION_REQUIRED,
-            '5.6'
-        );
+        esc_html__(
+            'Event Espresso Recurring Events addon could not be activated. Please ensure that Event Espresso version %1$s or higher is activated and your server is running PHP version %2$s or greater.',
+            'event_espresso'
+        ),
+        EE_REM_CORE_VERSION_REQUIRED,
+        '5.6'
+    );
     ?>
     <div class="error">
         <p><?php echo $error_message; ?></p>
