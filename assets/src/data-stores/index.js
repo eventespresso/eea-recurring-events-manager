b2f8b4c6a33172a8d8d5259bc070de20a51b38b4
Brent Christensen <brent@eventespresso.com>
2018-08-24 12:16:49 -0700

    convert store to use WP Redux wrapper

--- assets/src/data-stores/index.js
+++ assets/src/data-stores/index.js
@@ -1,28 +1,24 @@
 /**
- * External dependencies
+ * WordPress dependencies
  */
-import { createStore, combineReducers } from 'redux';
+import { registerStore, combineReducers } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { rRuleReducer, STORE_KEY_RRULE } from './rRule/reducer.js';
-import { exRuleReducer, STORE_KEY_EXRULE } from './exRule/reducer.js';
-import { rDatesReducer, STORE_KEY_RDATES } from './rDates/reducer.js';
-import { exDatesReducer, STORE_KEY_EXDATES } from './exDates/reducer.js';
+import { remActions } from './actions';
+import { rRuleReducer } from './rRule/reducer';
+import { exRuleReducer } from './exRule/reducer';
+import { rDatesReducer } from './rDates/reducer';
+import { exDatesReducer } from './exDates/reducer';
 
-const remReducers = combineReducers( {
-	rRuleReducer,
-	exRuleReducer,
-	rDatesReducer,
-	exDatesReducer,
-} );
-
-const DEFAULT_STATE = {};
-DEFAULT_STATE[ STORE_KEY_RRULE ] = '';
-DEFAULT_STATE[ STORE_KEY_EXRULE ] = '';
-DEFAULT_STATE[ STORE_KEY_RDATES ] = [];
-DEFAULT_STATE[ STORE_KEY_EXDATES ] = [];
-
-const remStore = createStore( remReducers, DEFAULT_STATE );
-export default remStore;
+export const remStore = registerStore(
+	'espressoRemStore',
+	combineReducers( {
+		rRuleReducer,
+		exRuleReducer,
+		rDatesReducer,
+		exDatesReducer,
+	} ),
+	remActions
+);
