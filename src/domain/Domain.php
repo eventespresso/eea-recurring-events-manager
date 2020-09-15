c7c0e39b26527f06a5010e27e24fae3121df3369
Brent Christensen <brent@eventespresso.com>
2020-09-14 18:32:12 -0700

    Load Barista Built Assets (#24)

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -2,7 +2,6 @@
 
 namespace EventEspresso\RecurringEvents\src\domain;
 
-use DomainException;
 use EventEspresso\core\domain\DomainBase;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
@@ -24,109 +23,4 @@ class Domain extends DomainBase
      */
     const CORE_VERSION_REQUIRED = EE_REM_CORE_VERSION_REQUIRED;
 
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminPath()
-    {
-        return $this->pluginPath() . 'domain/services/admin/recurring_events/';
-    }
-
-
-
-    /**
-     * @return string
-     */
-    public function adminPageSlug()
-    {
-        return 'espresso_recurring_events';
-    }
-
-
-
-    /**
-     * @return string
-     */
-    public function adminPageLabel()
-    {
-        return esc_html__('Recurring Events', 'event_espresso');
-    }
-
-
-
-    /**
-     * @return string
-     */
-    public function adminPageUrl()
-    {
-        return admin_url('admin.php?page=' . $this->adminPageSlug());
-    }
-
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminAssetsPath()
-    {
-        return $this->adminPath() . 'assets/';
-    }
-
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminTemplatePath()
-    {
-        return $this->adminPath() . 'templates/';
-    }
-
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminBaseUrl()
-    {
-        return $this->pluginUrl() . 'admin/recurring_events/';
-    }
-
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminAssetsUrl()
-    {
-        return $this->adminBaseUrl() . 'assets/';
-    }
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminTemplateUrl()
-    {
-        return $this->adminBaseUrl() . 'templates/';
-    }
-
-
-
-    /**
-     * @return string
-     * @throws DomainException
-     */
-    public function adminUiPath()
-    {
-        return $this->pluginPath() . 'ui/admin/';
-    }
 }
--- src/domain/services/assets/RecurringEventsAssetManager.php
+++ src/domain/services/assets/RecurringEventsAssetManager.php
@@ -3,84 +3,32 @@
 namespace EventEspresso\RecurringEvents\src\domain\services\assets;
 
 use DomainException;
-use EventEspresso\core\domain\services\assets\AdminRefactorAssetManager;
-use EventEspresso\core\exceptions\InvalidDataTypeException;
-use EventEspresso\core\exceptions\InvalidEntityException;
-use EventEspresso\core\services\assets\AssetManager;
-use EventEspresso\core\services\collections\DuplicateCollectionIdentifierException;
+use EventEspresso\core\domain\Domain;
+use EventEspresso\core\domain\services\assets\ReactAssetManager;
 
 /**
  * Class RecurringEventsAssetManager
- * Description
  *
  * @package EventEspresso\RecurringEvents\src\domain\services\assets\RecurringEventsAssetManager
  * @author  Brent Christensen
  *
  * @since   $VID:$
  */
-class RecurringEventsAssetManager extends AssetManager
+class RecurringEventsAssetManager extends ReactAssetManager
 {
-    const JS_HANDLE_REM_APP = 'eea-recurring-events-manager-app';
-    const CSS_HANDLE_REM_APP = 'eea-recurring-events-manager-app';
-    const ASSET_CHUNK_NAME = 'recurring-events-manager-app';
+    const DOMAIN = 'rem';
 
-    /**
-     * @since 4.9.62.p
-     * @throws DomainException
-     * @throws InvalidDataTypeException
-     * @throws InvalidEntityException
-     * @throws DuplicateCollectionIdentifierException
-     */
-    public function addAssets()
-    {
-        $this->registerJavascript();
-        $this->registerStyleSheets();
-    }
+    const ASSET_HANDLE = Domain::ASSET_NAMESPACE . RecurringEventsAssetManager::DOMAIN;
 
 
     /**
-     * Register javascript assets
-     *
      * @throws DomainException
-     * @throws InvalidDataTypeException
-     * @throws InvalidEntityException
-     * @throws DuplicateCollectionIdentifierException
      */
-    private function registerJavascript()
+    public function enqueueEventEditor()
     {
-        $this->addJavascript(
-            self::JS_HANDLE_REM_APP,
-            $this->registry->getJsUrl(
-                $this->domain->assetNamespace(),
-                self::ASSET_CHUNK_NAME
-            ),
-            [
-                AdminRefactorAssetManager::JS_HANDLE_EDITOR,
-                // self::JS_HANDLE_RRULE
-            ]
-        )
-            ->setRequiresTranslation()
-            ->setVersion($this->domain->version());
-    }
-
-
-    /**
-     * Register CSS assets.
-     *
-     * @throws DomainException
-     * @throws DuplicateCollectionIdentifierException
-     * @throws InvalidDataTypeException
-     * @throws InvalidEntityException
-     */
-    private function registerStyleSheets()
-    {
-        $this->addStylesheet(
-            self::CSS_HANDLE_REM_APP,
-            $this->registry->getCssUrl(
-                $this->domain->assetNamespace(),
-                self::ASSET_CHUNK_NAME
-            ),
-            [ AdminRefactorAssetManager::CSS_HANDLE_EDITOR ]
-        );
+        if ($this->verifyAssetIsRegistered(RecurringEventsAssetManager::ASSET_HANDLE)) {
+            wp_enqueue_script(RecurringEventsAssetManager::ASSET_HANDLE);
+            wp_enqueue_style(RecurringEventsAssetManager::ASSET_HANDLE);
+        }
     }
 }
