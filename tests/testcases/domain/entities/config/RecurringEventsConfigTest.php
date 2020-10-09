9bf6eb0b30869274757b7731c44dea49411833e5
Brent Christensen <brent@eventespresso.com>
2020-10-09 13:39:55 -0700

    reconfigure tests placeholder

---
+++ tests/testcases/domain/entities/config/RecurringEventsConfigTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EventEspresso\RecurringEvents\tests\testcases\domain\entities\config;
+
+use PHPUnit\Framework\TestCase;
+
+class RecurringEventsConfigTest extends TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+
+    public function testNothing()
+    {
+		$yup = true;
+        $this->assertTrue($yup);
+    }
+}
