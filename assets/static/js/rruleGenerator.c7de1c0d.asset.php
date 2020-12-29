d418ff8fc343fa80ecb9a1044f1d05c6c8d9445d
eebot <team@eventespresso.com>
2020-12-29 12:21:36 +0000

    Deployed from eventespresso/barista@810e99c7681fa0e2eda5e844d6c664a0def8ec34

--- assets/static/js/rruleGenerator.c7de1c0d.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('eventespresso-adapters', 'eventespresso-constants', 'eventespresso-dates', 'eventespresso-i18n', 'eventespresso-uiComponents', 'react'), 'version' => 'd4fd3097bb6873e62ccac7196f667bb2');
--- languages/event_espresso-translations-js.php
+++ languages/event_espresso-translations-js.php
@@ -1,6 +1,39 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $generated_i18n_strings = array(
+	// Reference: domains/rem/src/constants/index.ts:19
+	__( 'Daily', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:20
+	__( 'Weekly', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:21
+	__( 'Monthly', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:35
+	__( 'Monday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:36
+	__( 'Tuesday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:37
+	__( 'Wednesday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:38
+	__( 'Thursday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:39
+	__( 'Friday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:40
+	__( 'Saturday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/constants/index.ts:41
+	__( 'Sunday', 'event_espresso' ),
+
+	// Reference: domains/rem/src/hooks/useDatesBulkEditActions.ts:23
+	__( 'add tickets', 'event_espresso' ),
+
 	// Reference: domains/rem/src/ui/EntityOptionsRow/index.tsx:21
 	__( 'datetime', 'event_espresso' ),
 
@@ -21,12 +54,24 @@ $generated_i18n_strings = array(
 	// Reference: domains/rem/src/ui/EntityOptionsRow/index.tsx:47
 	__( 'Add New', 'event_espresso' ),
 
+	// Reference: domains/rem/src/ui/Modal/Modal.tsx:14
+	__( 'Recurring Events Manager', 'event_espresso' ),
+
 	// Reference: domains/rem/src/ui/Tickets/TicketTemplate.tsx:33
 	// Reference: domains/rem/src/ui/datetimeDetails/DateTemplate.tsx:21
 	__( 'Select…', 'event_espresso' ),
 
 	// Reference: domains/rem/src/ui/Tickets/TicketTemplate.tsx:45
 	__( 'Add', 'event_espresso' ),
+
+	// Reference: domains/rem/src/ui/bulkAddTickets/Container.tsx:44
+	__( 'Bulk Add Tickets', 'event_espresso' ),
+
+	// Reference: domains/rem/src/ui/bulkAddTickets/Footer.tsx:15
+	__( 'Cancel', 'event_espresso' ),
+
+	// Reference: domains/rem/src/ui/bulkAddTickets/Footer.tsx:17
+	__( 'Submit', 'event_espresso' ),
 
 	// Reference: domains/rem/src/ui/datetimeDetails/DateTemplate.tsx:29
 	__( 'Select', 'event_espresso' )
