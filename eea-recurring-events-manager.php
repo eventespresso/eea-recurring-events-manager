685f22b58cc34243e6fa02e71f5417f88ceda246
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-09-14 21:41:49 +0530

    🐛 Fix EEM_Recurrence field defaults (#59)
    * :bug: Fix EEM_Recurrence field defaults
    
    * Fix lints

--- eea-recurring-events-manager.php
+++ eea-recurring-events-manager.php
@@ -95,9 +95,9 @@ function eea_recurring_events_activation_error($error_message = '')
             deactivate_plugins(plugin_basename(__FILE__));
             ?>
 <div class="error">
-	<p><?php echo $error_message; ?></p>
+    <p><?php echo $error_message; ?></p>
 </div>
-<?php
+            <?php
         }
     );
 }
