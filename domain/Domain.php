6d03eeeb66e83102fcdf62e92f95f5fc3642382e
Brent Christensen <brent@eventespresso.com>
2022-06-08 09:03:19 -0700

    add constant for REM cap

--- domain/Domain.php
+++ domain/Domain.php
@@ -18,4 +18,9 @@ class Domain extends DomainBase
      * EE Core Version Required for Add-on
      */
     const CORE_VERSION_REQUIRED = EE_REM_CORE_VERSION_REQUIRED;
+
+    /**
+     * User Capability Required for Add-on
+     */
+    const USER_CAP_REQUIRED = 'ee_recurring_events_manager';
 }
